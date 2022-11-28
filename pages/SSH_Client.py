793e42f72af11d2681dbe358fd00f27e4ce999c5
christianperauer <christianperauer@yahoo.com>
2022-11-28 00:23:18 -0500

    Adding SSH Client and renaming CLI data collection file
    Created SSH_Client file to enable interactive ssh client
    Renamed Device_SSH to Gather_device_data to retrieve specific commands and configurations

---
+++ pages/SSH_Client.py
@@ -0,0 +1,23 @@
+import paramiko
+
+hostname = '172.20.20.2'
+port = 22
+user = 'admin'
+passwd = 'admin'
+
+try:
+    client = paramiko.SSHClient()
+    client.load_system_host_keys()
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    client.connect(hostname, port=port, username=user, password=passwd)
+    while True:
+        try:
+            cmd = input(f'{hostname} - $> ')
+            if cmd == 'exit': break
+            stdin, stdout, stderr = client.exec_command(cmd)
+            print(stdout.read().decode())
+        except KeyboardInterrupt:
+            break
+    client.close()
+except Exception as err:
+    print(std(err))
