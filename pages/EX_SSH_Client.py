11c799f84dea77a6a65096c3f9130486883a7740
christianperauer <christianperauer@yahoo.com>
2022-11-28 10:24:47 -0500

    Create EX_SSH_Client.py
    Testing SSH client with Exscript library

---
+++ pages/EX_SSH_Client.py
@@ -0,0 +1,20 @@
+from Exscript.util.interact import read_login
+from Exscript.protocols import SSH2
+
+
+account = read_login()
+conn = SSH2()                       
+conn.connect('172.20.20.2')     
+conn.login(account)  
+
+
+while True:
+    command = raw_input('cli: ')
+    if command == 'q': break
+    conn.execute(command)
+    print conn.response
+
+
+
+conn.send('quit\r')               
+conn.close() 
