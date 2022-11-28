8acc80731e3f9521a3bd4323f86e0416b6d93b2c
christianperauer <christianperauer@yahoo.com>
2022-11-28 16:12:49 -0500

    Update SSH_Client.py
    Adding tuple variable to the enable command statement

--- pages/SSH_Client.py
+++ pages/SSH_Client.py
@@ -1,4 +1,5 @@
 import paramiko
+from paramiko import SSHClient
 import time
 
 hostname = '172.20.20.2'
@@ -7,11 +8,11 @@ user = 'admin'
 passwd = 'admin'
 
 try:
-    client = paramiko.SSHClient()
+    client = SSHClient()
     client.load_system_host_keys()
     client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     client.connect(hostname, port=port, username=user, password=passwd)
-    client.exec_command('enable')
+    stdin, stdout, stderr = client.exec_command('enable')
     while True:
         try:
             cmd = input(f'{hostname} - $> ')
