158d45c370a00bcf68cc351fa6e35197e34df3f4
christianperauer <christianperauer@yahoo.com>
2022-11-28 18:22:34 -0500

    Remove SSH 2 and wrapping SSH into function
    Removing Client 2 File and Wrapping SSH client into a Function to move it to Utils later

--- pages/SSH_Client.py
+++ pages/SSH_Client.py
@@ -1,26 +1,29 @@
 import paramiko
 from paramiko import SSHClient
 import time
+import streamlit as st
 
-hostname = '172.20.20.2'
-port = 22
-user = 'arista'
-passwd = 'arista'
+def connect_to_device():
 
-try:
-    client = SSHClient()
-    client.load_system_host_keys()
-    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    client.connect(hostname, port=port, username=user, password=passwd)
-    while True:
-        try:
-            cmd = input(f'{hostname} - $> ')
-            if cmd == 'exit':
+    hostname = '172.20.20.2'
+    port = 22
+    user = 'arista'
+    passwd = 'arista'
+
+    try:
+        client = SSHClient()
+        client.load_system_host_keys()
+        client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        client.connect(hostname, port=port, username=user, password=passwd)
+        while True:
+            try:
+                cmd = input(f'{hostname} - $> ')
+                if cmd == 'exit':
+                    break
+                stdin, stdout, stderr = client.exec_command(cmd)
+                print(stdout.read().decode())
+            except KeyboardInterrupt:
                 break
-            stdin, stdout, stderr = client.exec_command(cmd)
-            print(stdout.read().decode())
-        except KeyboardInterrupt:
-            break
-    client.close()
-except Exception as err:
-    print(std(err))
+        client.close()
+    except Exception as err:
+        print(std(err))
