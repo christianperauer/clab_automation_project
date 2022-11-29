07cf58a08f1a2b038684a8f3b6c3566940f5333b
christianperauer <christianperauer@yahoo.com>
2022-11-29 02:10:15 -0500

    Including the SSH client script into App
    Added main SSH client function into Utils and created SSH Clients own page

--- pages/SSH_Client.py
+++ pages/SSH_Client.py
@@ -1,29 +1,34 @@
-import paramiko
-from paramiko import SSHClient
-import time
 import streamlit as st
+from utils import load_lab, check_run, destroy_lab, connect_to_device
 
-def connect_to_device():
+def ssh_to_dev():
+    with st.form("ssh connection"):
+        st.write("Lab Devices")
+        option = st.selectbox(
+            'Select a Device:', ('', '172.20.20.2', '172.20.20.3')
+        )
+        submitted = st.form_submit_button("Connect")
+        if submitted:
+            with st.spinner(text="Conecting to device %s" % option):
+                opt_check = check_run(option)
+            if opt_check.returncode == 0 and opt_check.stdout == '':
+                with st.spinner(text="Connecting..."):
+                    ssh_session = connect_to_device(option)
+                st.success("Connected!")
+                if ssh_session.returncode == 0:
+                    st.success('Connected via SSH successfully!', icon="✅")
+                    with st.expander("Connection details"):
+                        st.code(ssh_session.stdout)
+                elif ssh_session.returncode == 1:
+                    st.error("There was a problem connecting to the device")
+                    with st.expander("Connection logs"):
+                        st.text(ssh_session.stderr)
+            elif opt_check.returncode == 0 and opt_check.stdout is not None:
+                st.warning('Connection already established', icon="⚠️")
+                with st.expander("Connection details"):
+                    st.code(opt_check.stdout)
+            elif opt_check.returncode == 1:
+                st.error('Error checking SSH connection', icon="🚨")
 
-    hostname = '172.20.20.2'
-    port = 22
-    user = 'arista'
-    passwd = 'arista'
-
-    try:
-        client = SSHClient()
-        client.load_system_host_keys()
-        client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        client.connect(hostname, port=port, username=user, password=passwd)
-        while True:
-            try:
-                cmd = input(f'{hostname} - $> ')
-                if cmd == 'exit':
-                    break
-                stdin, stdout, stderr = client.exec_command(cmd)
-                print(stdout.read().decode())
-            except KeyboardInterrupt:
-                break
-        client.close()
-    except Exception as err:
-        print(std(err))
+if __name__ == "__main__":
+    ssh_to_dev()
--- utils.py
+++ utils.py
@@ -1,5 +1,9 @@
 import streamlit as st
 import subprocess
+import time
+import paramiko
+from paramiko import SSHClient
+
 
 def load_lab(lab_option):
     if lab_option == 'Arista':
@@ -12,3 +16,29 @@ def check_run(lab_option):
 def destroy_lab(lab_option):
     if lab_option == 'Arista':
         return subprocess.run(['sudo', 'containerlab', 'destroy', '-t', 'arista.labtest.yml'], text=True, check=True, capture_output=True)
+
+def connect_to_device(dev_option):
+
+    hostname = dev_option
+    port = 22
+    user = 'arista'
+    passwd = 'arista'
+
+    try:
+        client = SSHClient()
+        client.load_system_host_keys()
+        client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        client.connect(hostname, port=port, username=user, password=passwd)
+        while True:
+            try:
+                cmd = input(f'{hostname} - $> ')
+                if cmd == 'exit':
+                    break
+                stdin, stdout, stderr = client.exec_command(cmd)
+                return st.code(stdout)
+                print(stdout.read().decode())
+            except KeyboardInterrupt:
+                break
+        client.close()
+    except Exception as err:
+        print(std(err))
